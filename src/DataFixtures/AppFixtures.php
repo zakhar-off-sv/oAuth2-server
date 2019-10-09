182f2835c7926918d11d0070e05d5a218a3b0e20
Sergey Zakharov <zakhar.off.sv@gmail.com>
2019-10-09 15:39:06 +0300

    fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Domain\Model\Client;
+use App\Domain\Model\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            $user = User::create("email-{$i}@dot818.com", "User{$i} Name");
+            $password = $this->encoder->encodePassword($user, "pass_{$i}");
+            $user->setPassword($password);
+            $user->setRoles(['user']);
+            $user->setActive(rand(0, 1) ? true : false);
+            $manager->persist($user);
+        }
+
+        for ($i = 1; $i <= 5; $i++) {
+            $client = Client::create("Client {$i}");
+            $client->setSecret(substr(md5($client->getName()), -10));
+            $client->setRedirect('https://google.com/cb');
+            $client->setActive(rand(0, 1) ? true : false);
+            $manager->persist($client);
+        }
+
+        $manager->flush();
+    }
+}

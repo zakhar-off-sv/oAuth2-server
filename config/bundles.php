3cc2bf75264021e3e49f1354dd6393659f2e1cad
Sergey Zakharov <zakhar.off.sv@gmail.com>
2019-10-15 10:04:08 +0300

    debug pack

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
